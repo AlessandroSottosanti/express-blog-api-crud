c08a587557c7dbd538ff5316b1fc663913b14ebe
AlessandroSottosanti <asottosanti2@gmail.com>
2025-01-08 18:43:12 +0100

    refactoring: destroy tag method

--- controllers/tagController.js
+++ controllers/tagController.js
@@ -9,10 +9,10 @@ const index = (req, res) => {
         post.tags.forEach(tag => {
 
             !tagsArray.includes(tag.toLocaleLowerCase()) &&
-            tagsArray.push(tag.toLowerCase()); // Aggiunge i tag in minuscolo
+                tagsArray.push(tag.toLowerCase()); // Aggiunge i tag in minuscolo
         });
     });
-   
+
     // ordinamento alfabetico
     tagsArray.sort();
 
@@ -28,16 +28,25 @@ const index = (req, res) => {
 // destroy
 const destroy = (req, res) => {
 
-    const idTagToDelete = req.id;
+    const tagToDelete = req.query.tag?.toLowerCase();
+    let tagFound = false;
 
     postsList.forEach(post => {
-        post.tags.findIndex(tag => {
-            tag.id === idTagToDelete;
-        });
+        // Trova l'indice del tag nell'array dei tag del post
+        const tagIndex = post.tags.findIndex(tag => tag.toLowerCase() === tagToDelete);
+        if (tagIndex !== -1) {
+            post.tags.splice(tagIndex, 1);
+            tagFound = true;
+        }
     });
 
-    idTagToDelete === -1 ? res.status(404).json({ error: "Tag non trovato" }) : res.status(204).json({ success: "Tag rimosso con successo"});
-    
+    if (!tagFound) { 
+        res.status(404).json({ error: `Tag '${tagToDelete}' non trovato` }) ;
+    }
+    else {
+        res.status(201).json({ success: `Tag '${tagToDelete}' rimosso con successo` });
+    }
+
 }
 
-export default { index, destroy };
+    export default { index, destroy };
